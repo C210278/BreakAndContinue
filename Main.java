11022025bd62cf8b2e3e201eb336ed8d333b52b5
ITO Eigo <itoh@ccg.ac.jp>
2021-11-08 19:10:51 +0900

    問題3の解答例

--- Main.java
+++ Main.java
@@ -3,12 +3,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("START");
         for (int num = 0; num < 15; num++) {
-            if(num == 9) { break; }
-            else if(num % 3 == 0) { continue; }
-            else if(num == 6) { break; }
+            if(num == 11) { break; }
+            else if(num % 4 == 0) { continue; }
+            else if(num == 5) { continue; }
             System.out.println(num);
         }
         System.out.println("END");            
     }
-    
+
 }
